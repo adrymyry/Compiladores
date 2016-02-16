08fd42b18288d19336a26c79c8a77bfa3bbbc248
Adrian Miralles <adrymyry@gmail.com>
2016-02-16 13:50:55 +0100

    Primera version

---
+++ lexico.h
@@ -0,0 +1,19 @@
+#ifndef _LEXICO_H
+#define _LEXICO_H
+
+#define ID 			1
+#define MAIN		2
+#define PRINT		3
+#define NUM 		4
+#define IGUAL		5
+#define MAS			6
+#define MENOS 	7
+#define PARI 		8
+#define PARD 		9
+#define PYC 		10
+#define COMA 		11
+#define LLAVEI 	12
+#define LLAVED 	13
+#define CADENA  14
+
+#endif
---
+++ makefile
@@ -0,0 +1,11 @@
+prueba: lex.yy.c main.c
+		gcc lex.yy.c main.c -lfl -o prueba
+
+lex.yy.c: lexico.l lexico.h
+		flex --yylineno lexico.l
+
+clean:
+		rm -f prueba lex.yy.c
+
+run: prueba prueba.txt
+		./prueba prueba.txt
