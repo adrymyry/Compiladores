1f48f43c20bdf5c739bf94cb7da5462fa59d42b2
Adrian Miralles <adrymyry@gmail.com>
2016-02-16 17:47:44 +0100

    Cambiados tabs para ver mejor el codigo. Borrados ficheros generados (make clean)

--- lexico.h
+++ lexico.h
@@ -7,13 +7,13 @@
 #define NUM 		4
 #define IGUAL		5
 #define MAS			6
-#define MENOS 	7
+#define MENOS       7
 #define PARI 		8
 #define PARD 		9
 #define PYC 		10
 #define COMA 		11
-#define LLAVEI 	12
-#define LLAVED 	13
-#define CADENA  14
+#define LLAVEI      12
+#define LLAVED      13
+#define CADENA      14
 
 #endif
--- main.c
+++ main.c
@@ -7,22 +7,23 @@ extern int yylex();
 
 int main(int argc, char const *argv[]) {
 
-  if (argc != 2) {
-    printf("Uso: %s fichero\n", argv[0]);
-    exit(1);
-  }
+    if (argc != 2) {
+        printf("Uso: %s fichero\n", argv[0]);
+        exit(1);
+    }
 
-  FILE *f_in = fopen(argv[1], "r");
-  if (f_in == NULL) {
-    printf("Archivo %s no existe\n", argv[1]);
-    exit(2);
-  }
+    FILE *f_in = fopen(argv[1], "r");
+    if (f_in == NULL) {
+        printf("Archivo %s no existe\n", argv[1]);
+        exit(2);
+    }
 
-  yyin = f_in;
-  int token;
-  while (token = yylex()) {
-    printf("Token: %d\n", token);
-  }
-  fclose(f_in);
-  return 0;
+    yyin = f_in;
+    int token;
+    while (token = yylex()) {
+        printf("Token: %d\n", token);
+    }
+
+    fclose(f_in);
+    return 0;
 }
