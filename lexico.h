1357b31691f83ec11adc116e068cbca122ba7c77
Adrian Miralles <adrymyry@gmail.com>
2016-02-23 13:53:28 +0100

    Fin clase 23/02

--- lexico.h
+++ lexico.h
@@ -1,19 +1,32 @@
 #ifndef _LEXICO_H
 #define _LEXICO_H
 
-#define ID 			1
-#define MAIN		2
-#define PRINT		3
-#define NUM 		4
-#define IGUAL		5
-#define MAS			6
-#define MENOS       7
-#define PARI 		8
-#define PARD 		9
-#define PYC 		10
-#define COMA 		11
-#define LLAVEI      12
-#define LLAVED      13
-#define CADENA      14
+#define PROGRAMA	1
+#define VAR         2
+#define ENTERO		3
+#define COMIENZO	4
+#define FIN         5
+#define SI			6
+#define ENTONCES    7
+#define SINO 		8
+#define MIENTRAS 	9
+#define HACER 		10
+#define IMPRIMIR	11
+#define LEER        12
+#define ID          13
+#define NUM 		14
+#define CADENA      15
+#define PYC 		16
+#define DOSP		17
+#define PUNTO       18
+#define COMA 		19
+#define MAS 		20
+#define MENOS 		21
+#define MULT 		22
+#define PARI        23
+#define PARD        24
+#define DIV         25
+#define ASSIGN      26
+
 
 #endif
